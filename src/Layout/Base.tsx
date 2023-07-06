6de1d821fba8a22dc4ef1b75398d35404c2f4f1b
Thanh Nguyen <908739+tnsicdr@users.noreply.github.com>
2023-07-06 12:15:36 -0400

    chore: Initialize AppBar

--- src/Layout/Base.tsx
+++ src/Layout/Base.tsx
@@ -1,8 +1,10 @@
 import { PropsWithChildren } from "react";
+import { AppBar } from "../components/AppBar/AppBar";
 
 function BaseLayout({ children }: PropsWithChildren) {
   return (
     <div className="font-sans bg-slate-100 text-slate-900 dark:bg-slate-900 dark:text-slate-100 h-screen w-screen">
+      <AppBar />
       <div className="w-full container mx-auto px-4">
         <main>{children}</main>
       </div>
---
+++ src/components/AppBar/AppBar.tsx
@@ -0,0 +1,9 @@
+export function AppBar() {
+  return (
+    <nav className="w-full bg-slate-200 dark:bg-slate-950">
+      <div className="mx-auto container px-4">
+        <div>GrimRoster</div>
+      </div>
+    </nav>
+  );
+}
