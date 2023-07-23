c1b70884f57819142bbfa39d48551d9259e62edf
Thanh Nguyen <908739+tnsicdr@users.noreply.github.com>
2023-07-23 17:52:03 -0400

    chore: Add PWA configs

--- vite.config.ts
+++ vite.config.ts
@@ -7,15 +7,34 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      injectRegister: "auto",
       registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
       devOptions: {
         enabled: true,
       },
       manifest: {
-        name: 'GrimRoster',
-        short_name: 'GrimRoster',
-        description: 'Grimdark Roster Management'
-      }
+        name: "GrimRoster",
+        short_name: "GrimRoster",
+        description: "Grimdark Roster Management",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "public/icons/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "public/icons/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
     }),
   ],
 });
